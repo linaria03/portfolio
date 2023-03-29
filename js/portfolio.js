c64189002a6cef092406f7490b2b1b0da62619f6
linaria03 <daseul9155@gmail.com>
2023-03-29 08:08:08 +0000

    projectA 업로드

---
+++ js/portfolio.js
@@ -0,0 +1,19 @@
+// 💛 원페이지 스크롤 애니매이션
+var mHtml = $("html");
+var page = 1;
+
+
+mHtml.animate({scrollTop : 0},10);
+
+$(window).on("wheel", function(e) {
+    if(mHtml.is(":animated")) return;
+    if(e.originalEvent.deltaY > 0) {
+        if(page == 4) return;
+        page++;
+    } else if(e.originalEvent.deltaY < 0) {
+        if(page == 1) return;
+        page--;
+    }
+    var posTop =(page-1) * $(window).height();
+    mHtml.animate({scrollTop : posTop});
+})
